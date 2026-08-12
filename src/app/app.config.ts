77e5122a74f516bb2dd76286910b403ba79185f3
Abdoul-Aziz ZOROM <abdoulazizzorom@gmail.com>
2026-08-12 16:41:47 +0000

    chore: installe PrimeNG 21, le theme Aura et l'integration Tailwind

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import { MessageService } from 'primeng/api';
+import { ConfirmationService } from 'primeng/api';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: { darkModeSelector: '.app-dark' },
+      },
+    }),
+    MessageService,
+    ConfirmationService,
+  ],
 };
